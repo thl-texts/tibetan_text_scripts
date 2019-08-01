b32b31ccf3fd0026bba4b0f0b8ca231cd4ea2817
unknown <than@virginia.edu>
2019-08-01 11:53:08 -0400

    Update to convertSamRTF.bat from windows, adding quote marks for wonky filenames

--- convertSamRTF.bat
+++ convertSamRTF.bat
@@ -2,7 +2,7 @@
 
 for %%f in (in\*.rtf) do (
     echo Doing %%~nf
-    copy %%f out\%%~nf.rtf
-    c:\unicdocp\udp.exe -x rtf out\%%~nf.rtf
-    del out\%%~nf.BAK
+    copy "%%f" "out\%%~nf.rtf"
+    c:\unicdocp\udp.exe -x rtf "out\%%~nf.rtf"
+    del "out\%%~nf.BAK"
 )
